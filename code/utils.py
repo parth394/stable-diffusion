906bde0fdb088f7c9c2e0919fccb4aec8352dfa0
Parth Pandey <parth.pandey13103447@gmail.com>
2024-04-06 01:02:52 +0530

    Training Update

---
+++ code/utils.py
@@ -0,0 +1,10 @@
+def rescale(x, old_range, new_range, clamp=False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+    x -= old_min
+    x *= (new_max - new_min) / (old_max - old_min)
+    x += new_min
+
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
